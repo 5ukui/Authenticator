cf417c3312222d21a836e589daa24c3841aa07b5
5ukui <m.huthaifa@proton.me>
2024-12-25 03:55:36 +0400

    Added homescreen and about screen animations

--- app/src/main/java/com/sukui/authenticator/ui/screen/about/AboutScreen.kt
+++ app/src/main/java/com/sukui/authenticator/ui/screen/about/AboutScreen.kt
@@ -2,24 +2,30 @@ package com.sukui.authenticator.ui.screen.about
 
 import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,6 +42,19 @@ fun AboutScreen(
     modifier: Modifier = Modifier,
     onBack: () -> Unit,
 ) {
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 4000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
     val uriHandler = rememberUriHandler()
     BackHandler(onBack = onBack)
     Scaffold(
@@ -63,7 +82,9 @@ fun AboutScreen(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Image(
-                modifier = Modifier.padding(0.dp),
+                modifier = Modifier
+                    .padding(0.dp)
+                    .graphicsLayer(rotationZ = rotation),
                 painter = painterResource(R.mipmap.ic_launcher_foreground),
                 contentDescription = null,
             )
--- app/src/main/java/com/sukui/authenticator/ui/screen/home/state/HomeScreenEmpty.kt
+++ app/src/main/java/com/sukui/authenticator/ui/screen/home/state/HomeScreenEmpty.kt
@@ -1,35 +1,52 @@
 package com.sukui.authenticator.ui.screen.home.state
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.sukui.authenticator.R
 
 @Composable
 fun HomeScreenEmpty() {
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 4000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            modifier = Modifier.size(72.dp),
-            painter = painterResource(R.drawable.ic_empty_dashboard),
-            contentDescription = null
+        Image(
+            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(160.dp)
+                .graphicsLayer(rotationZ = rotation)
         )
-        ProvideTextStyle(MaterialTheme.typography.headlineSmall) {
-            Text(stringResource(R.string.home_dashboard_empty))
-        }
+        Text(
+            text = "Nothing here yet",
+            style = MaterialTheme.typography.bodyLarge, // More balanced style
+            fontWeight = FontWeight.Medium, // Slight emphasis
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f) // Softer gray tone
+        )
     }
 }
